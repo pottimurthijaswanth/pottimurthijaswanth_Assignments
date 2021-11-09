6e06a5f23f61860e8849d33f16c43e6e60ca439a
pottimurthijaswanth <pottimurthijaswanth@gmail.com>
2021-11-10 00:10:44 +0530

    Assignment5

---
+++ java_Assignment5/src/java_Assignment5/Collection2.java
@@ -0,0 +1,28 @@
+package java_Assignment5;
+
+import java.util.HashSet;
+
+public class Collection2 {
+	   public static void main(String args[]) {
+	      
+	      HashSet<String> hset = 
+	               new HashSet<String>();
+
+	     
+	      hset.add("Apple");
+	      hset.add("Mango");
+	      hset.add("Grapes");
+	      hset.add("Orange");
+	      hset.add("Fig");
+	      hset.add("DragonFruit");
+	      hset.add("JackFruit");
+	      hset.add("Banana");
+	      hset.add("PineApple");
+	      hset.add("Guava");
+	      
+	      hset.add("Apple");
+	      hset.add("Mango");
+	      
+	      System.out.println(hset);
+	    }
+	}
