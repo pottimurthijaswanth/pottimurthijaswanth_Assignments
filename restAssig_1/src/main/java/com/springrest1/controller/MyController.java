84d25b231a367f58c59861a488e68f96b6033a60
pottimurthijaswanth <93123286+pottimurthijaswanth@users.noreply.github.com>
2021-11-29 09:58:25 +0530

    rest Assignment 1

---
+++ restAssig_1/src/main/java/com/springrest1/controller/MyController.java
@@ -0,0 +1,15 @@
+package com.springrest1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	
+     @GetMapping("/hello")
+     public String helloworld() {
+     
+      return "Hello World";
+
+}
+}
